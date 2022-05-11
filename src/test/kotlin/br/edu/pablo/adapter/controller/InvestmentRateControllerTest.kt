465f51d2da7f8f690873cfe05ef6199be7d1da47
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-11 18:09:53 -0300

    test: implementando classe de testes para Taxa de Investimento

---
+++ src/test/kotlin/br/edu/pablo/adapter/controller/InvestmentRateControllerTest.kt
@@ -0,0 +1,38 @@
+package br.edu.pablo.adapter.controller
+
+import br.edu.pablo.adapter.service.InvestmentRateService
+import br.edu.pablo.factory.InvestmentRateFactory
+import br.edu.pablo.usecase.constant.ONCE
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+internal class InvestmentRateControllerTest {
+    private val investmentRateServiceMock = mockk<InvestmentRateService>()
+    private val investmentRateController = InvestmentRateController(investmentRateServiceMock)
+
+    @Test
+    fun mustGetInvestmentRateWithSuccess() {
+        every { investmentRateServiceMock.getUpdatedRate() } returns InvestmentRateFactory().createWithRandomPercentage()
+
+        investmentRateController.getUpdatedRate()
+
+        verify(exactly = ONCE) { investmentRateServiceMock.getUpdatedRate() }
+    }
+
+    @Test
+    fun mustGetInvestmentRateWithError() {
+        every { investmentRateServiceMock.getUpdatedRate() } throws NullPointerException()
+
+        Assert.assertThrows(NullPointerException::class.java) { investmentRateController.getUpdatedRate() }
+
+        verify(exactly = ONCE) { investmentRateServiceMock.getUpdatedRate() }
+    }
+}
