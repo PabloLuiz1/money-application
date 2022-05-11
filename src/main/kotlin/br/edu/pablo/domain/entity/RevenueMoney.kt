fe90097d9b10aedb637b064bd810437d2d78502e
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-11 16:51:32 -0300

    feat: adicionando classes de dominio para Rendimento e Taxa de Investimento

---
+++ src/main/kotlin/br/edu/pablo/domain/entity/RevenueMoney.kt
@@ -0,0 +1,26 @@
+package br.edu.pablo.domain.entity
+
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+
+@Entity
+class RevenueMoney(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    val id: Long? = null,
+
+    @ManyToOne
+    val customer: CustomerAccount,
+
+    @Column(nullable = false)
+    val amount: Double
+) {
+    @Column(nullable = false)
+    val updatedDate = LocalDateTime.now()
+}
