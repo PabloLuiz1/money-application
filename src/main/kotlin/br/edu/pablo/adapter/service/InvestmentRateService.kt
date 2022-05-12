11d68438d8e70c02fbe185c0be398daee47927ce
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-11 23:06:32 -0300

    feat: implementando métodos de CRUD e atualização de status

--- src/main/kotlin/br/edu/pablo/adapter/service/InvestmentRateService.kt
+++ src/main/kotlin/br/edu/pablo/adapter/service/InvestmentRateService.kt
@@ -15,5 +15,25 @@ class InvestmentRateService {
             null, "CDI", Random.nextDouble(PERCENTAGE_RATE_RANGE.first, PERCENTAGE_RATE_RANGE.second),
             InvestmentRateStatus(null, InvestmentRateParams.UPDATED.name)
         )
+        return save(newInvestmentRate)
+    }
+
+    fun save(investmentRate: InvestmentRate): InvestmentRate {
+        return investmentRateRepository.save(investmentRate)
+    }
+
+    fun findAll(): MutableList<InvestmentRate> {
+        return investmentRateRepository.findAll()
+    }
+
+    fun updateToOutdatedStatus() {
+        val outdatedStatus = investmentRateStatusService.findByDescription(InvestmentRateStatusParams.OUTDATED.name).get()
+        val investmentsRateToUpdate = findAll().filter { it.updatedDate.toLocalDate() < LocalDate.now() }
+        investmentsRateToUpdate.forEach {
+            it.status = outdatedStatus
+        }
+        investmentsRateToUpdate.forEach {
+            investmentRateRepository.save(it)
+        }
     }
 }
