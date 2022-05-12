51678333323902ee9982c7cd996cc1cea1b36565
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-11 23:39:51 -0300

    feat: implementando classe de repositório de taxa de investimento

---
+++ src/main/kotlin/br/edu/pablo/adapter/repository/InvestmentRateRepository.kt
@@ -0,0 +1,12 @@
+package br.edu.pablo.adapter.repository
+
+import br.edu.pablo.domain.entity.InvestmentRate
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
+
+@Repository
+interface InvestmentRateRepository : JpaRepository<InvestmentRate, Long> {
+
+    fun findInvestmentRateByUpdatedDateBefore(dateTime: LocalDateTime): MutableList<InvestmentRate>
+}
