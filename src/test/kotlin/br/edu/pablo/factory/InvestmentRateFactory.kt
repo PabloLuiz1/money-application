90f0ffbc2b1ae097aecd555fdd1a1392ee8a85b2
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-11 23:07:57 -0300

    feat: implementando método para criação de taxa desatualizada

--- src/test/kotlin/br/edu/pablo/factory/InvestmentRateFactory.kt
+++ src/test/kotlin/br/edu/pablo/factory/InvestmentRateFactory.kt
@@ -1,20 +1,27 @@
 package br.edu.pablo.factory
 
 import br.edu.pablo.domain.entity.InvestmentRate
-import br.edu.pablo.domain.entity.InvestmentRateParams
 import br.edu.pablo.domain.entity.InvestmentRateStatus
-import br.edu.pablo.usecase.constant.PERCENTAGE_RATE_RANGE
-import kotlin.random.Random
+import br.edu.pablo.domain.entity.InvestmentRateStatusParams
+import br.edu.pablo.usecase.constant.ONE_DAY
+import br.edu.pablo.usecase.constant.PERCENTAGE_RATE
+import java.time.LocalDateTime
 
 class InvestmentRateFactory {
 
     companion object {
         private const val ID = 1L
         private const val DESCRIPTION = "CDI"
-        private val STATUS = InvestmentRateStatus(1L, InvestmentRateParams.UPDATED.name)
+        private val STATUS_UPDATED = InvestmentRateStatus(1L, InvestmentRateStatusParams.UPDATED.name)
     }
 
-    fun createWithRandomPercentage(): InvestmentRate {
-        return InvestmentRate(ID, DESCRIPTION, Random.nextDouble(PERCENTAGE_RATE_RANGE.first, PERCENTAGE_RATE_RANGE.second), STATUS)
+    fun createWithDefaultPercentage(): InvestmentRate {
+        return InvestmentRate(ID, DESCRIPTION, PERCENTAGE_RATE, STATUS_UPDATED)
+    }
+
+    fun createOutdatedRate(): InvestmentRate {
+        val investmentRate = InvestmentRate(ID, DESCRIPTION, PERCENTAGE_RATE, STATUS_UPDATED)
+        investmentRate.updatedDate = LocalDateTime.now().minusDays(ONE_DAY)
+        return investmentRate
     }
 }
