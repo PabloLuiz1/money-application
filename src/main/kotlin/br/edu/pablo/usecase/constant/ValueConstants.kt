107982d02426662c5ce2c5ec2de1b7715798991f
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-25 16:18:16 -0300

    refactor: adicionando constantes para porcentagem randômica da taxa e formatação do dinheiro em BRL

--- src/main/kotlin/br/edu/pablo/usecase/constant/ValueConstants.kt
+++ src/main/kotlin/br/edu/pablo/usecase/constant/ValueConstants.kt
@@ -2,6 +2,10 @@ package br.edu.pablo.usecase.constant
 
 val NEVER: Int = 0
 val ONCE: Int = 1
-val TWICE: Int = 2
-val PERCENTAGE_RATE: Double = 6.7
+val ZERO_BRL: Double = 0.0
+val BRL_SCALE: Int = 2
+val ONE_HUNDRED = 100
+val PERCENTAGE_RATE: Double = 12.57
+val PERCENTAGE_RATE_MIN: Double = 10.23
+val PERCENTAGE_RATE_MAX: Double = 12.75
 val ONE_DAY: Long = 1
