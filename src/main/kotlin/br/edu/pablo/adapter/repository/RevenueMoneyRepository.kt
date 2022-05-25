0c46306ae99c5cba6fffed673d9cd4f634aa1fec
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-25 16:27:46 -0300

    feat: implementando classe de Repository para RevenueMoney

---
+++ src/main/kotlin/br/edu/pablo/adapter/repository/RevenueMoneyRepository.kt
@@ -0,0 +1,12 @@
+package br.edu.pablo.adapter.repository
+
+import br.edu.pablo.domain.entity.Customer
+import br.edu.pablo.domain.entity.RevenueMoney
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface RevenueMoneyRepository : JpaRepository<RevenueMoney, Long> {
+
+    fun findRevenueMoniesByCustomerAccount_Customer(customer: Customer): MutableList<RevenueMoney>
+}
