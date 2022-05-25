1062887a13467d774593916b04a739efb0d135ca
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-25 16:20:51 -0300

    refactor: adicionando Query para buscar um registro por InvestmentRateStatus

--- src/main/kotlin/br/edu/pablo/adapter/repository/InvestmentRateRepository.kt
+++ src/main/kotlin/br/edu/pablo/adapter/repository/InvestmentRateRepository.kt
@@ -1,6 +1,7 @@
 package br.edu.pablo.adapter.repository
 
 import br.edu.pablo.domain.entity.InvestmentRate
+import br.edu.pablo.domain.entity.InvestmentRateStatus
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
@@ -9,4 +10,5 @@ import java.time.LocalDateTime
 interface InvestmentRateRepository : JpaRepository<InvestmentRate, Long> {
 
     fun findInvestmentRateByUpdatedDateBefore(dateTime: LocalDateTime): MutableList<InvestmentRate>
+    fun findInvestmentRateByStatus(status: InvestmentRateStatus): InvestmentRate
 }
