a7b85f29762cdfe6e23a202b868a8a69f921baee
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-25 16:14:56 -0300

    data: adicionando INSERT`s para testes manuais

--- data/init-data.sql
+++ data/init-data.sql
@@ -5,3 +5,16 @@ INSERT INTO investment_rate_status(description, updated_date) VALUES ('DESATUALI
 -- CUSTOMER_GENDER
 INSERT INTO customer_gender(description) VALUES ('MASCULINO');
 INSERT INTO customer_gender(description) VALUES ('FEMININO');
+
+-- CUSTOMER
+INSERT INTO customer(birth_date, cpf, email, name, gender_id)
+VALUES ('1999-08-31', '12345678900', 'pabloluiz@gmail.com', 'Pablo Luiz Ribeiro', 1);
+
+--CUSTOMER_ACCOUNT
+INSERT INTO public.customer_account(balance, customer_id)
+VALUES (100.0, 1);
+
+--INVESTMENT_RATE
+INSERT INTO public.investment_rate(description, effective_date, percentage, updated_date, status_id)
+VALUES ('CDI', '2022-05-19', 0.047, NOW(), 1);
+
