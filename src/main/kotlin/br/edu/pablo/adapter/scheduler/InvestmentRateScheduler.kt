382754decd74dbe483c31898d2e14ecedf43eac9
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-25 23:42:23 -0300

    refactor: adicionando implantação da funcionalidade de Scheduler de obter InvestmentRate atualizada via API

--- src/main/kotlin/br/edu/pablo/adapter/scheduler/InvestmentRateScheduler.kt
+++ src/main/kotlin/br/edu/pablo/adapter/scheduler/InvestmentRateScheduler.kt
@@ -1,7 +1,9 @@
 package br.edu.pablo.adapter.scheduler
 
 import br.edu.pablo.adapter.controller.InvestmentRateController
+import br.edu.pablo.adapter.converter.InvestmentRateConverter
 import br.edu.pablo.adapter.service.InvestmentRateService
+import com.github.michaelbull.logging.InlineLogger
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -10,12 +12,24 @@ import org.springframework.stereotype.Component
 @EnableScheduling
 class InvestmentRateScheduler(
     val investmentRateController: InvestmentRateController,
-    val investmentRateService: InvestmentRateService
+    val investmentRateService: InvestmentRateService,
+    val investmentRateConverter: InvestmentRateConverter
 ) {
+
+    companion object {
+        private val LOGGER = InlineLogger(InvestmentRateScheduler::class)
+    }
 
     @Scheduled(cron = "\${cron.daily.investment.rate}")
     fun getUpdatedInvestmentRate() {
-        // val investmentRate = InvestmentRate(null, "CDI", investmentRateController.getUpdatedRate().)
-        // investmentRateService.save(investmentRateController.getUpdatedRate())
+        LOGGER.info { "Iniciando Job Scheduler para obter Taxa de Investimento vigente" }
+        val updatedInvestmentRateDto = investmentRateController.getUpdatedRate().body
+        try {
+            investmentRateService.save(investmentRateConverter.toEntityFromDto(updatedInvestmentRateDto!!))
+            LOGGER.info { "Encerrando Job Scheduler para obter Taxa de Investimento vigente" }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            LOGGER.error { "Erro ao tentar salvar a Taxa de Investimento vigente no banco de dados" }
+        }
     }
 }
