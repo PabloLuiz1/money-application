ed1942ea4d8d0e0d3d2628ae1e57109907f62a05
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-25 23:42:58 -0300

    refactor: implementando funcionalidades de buscar todos rendimentos e buscar rendimentos por cliente

--- src/main/kotlin/br/edu/pablo/adapter/controller/RevenueMoneyController.kt
+++ src/main/kotlin/br/edu/pablo/adapter/controller/RevenueMoneyController.kt
@@ -5,6 +5,8 @@ import br.edu.pablo.domain.dto.CustomerDto
 import br.edu.pablo.domain.entity.RevenueMoney
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,13 +16,13 @@ class RevenueMoneyController(
     val revenueMoneyService: RevenueMoneyService
 ) {
 
-    @GetMapping
+    @GetMapping("/all")
     fun findAll(): ResponseEntity<MutableList<RevenueMoney>> {
         return ResponseEntity.ok().body(revenueMoneyService.findAll())
     }
 
-    @GetMapping("/customer")
-    fun findByCustomer(customerDto: CustomerDto): ResponseEntity<MutableList<RevenueMoney>> {
+    @PostMapping("/customer")
+    fun findByCustomer(@RequestBody customerDto: CustomerDto): ResponseEntity<MutableList<RevenueMoney>> {
         return ResponseEntity.ok().body(revenueMoneyService.findByCustomer(customerDto.id))
     }
 }
