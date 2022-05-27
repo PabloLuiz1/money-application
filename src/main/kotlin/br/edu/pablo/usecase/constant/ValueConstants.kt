23354b13331bb232bd648c0d95d5409c469c4232
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-26 23:26:32 -0300

    refactor: atualizando constantes e adicionando a constante TWICE

--- src/main/kotlin/br/edu/pablo/usecase/constant/ValueConstants.kt
+++ src/main/kotlin/br/edu/pablo/usecase/constant/ValueConstants.kt
@@ -2,10 +2,9 @@ package br.edu.pablo.usecase.constant
 
 val NEVER: Int = 0
 val ONCE: Int = 1
+val TWICE: Int = 2
 val ZERO_BRL: Double = 0.0
 val BRL_SCALE: Int = 2
 val ONE_HUNDRED = 100
 val PERCENTAGE_RATE: Double = 12.57
-val PERCENTAGE_RATE_MIN: Double = 10.23
-val PERCENTAGE_RATE_MAX: Double = 12.75
 val ONE_DAY: Long = 1
