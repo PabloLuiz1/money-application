859702f20b008bea65ea1bc8655305d992c73ea9
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-05-26 23:26:03 -0300

    refactor: removendo o uso do saveAll e atualizando nome de variável

--- src/main/kotlin/br/edu/pablo/adapter/service/InvestmentRateService.kt
+++ src/main/kotlin/br/edu/pablo/adapter/service/InvestmentRateService.kt
@@ -2,14 +2,9 @@ package br.edu.pablo.adapter.service
 
 import br.edu.pablo.adapter.repository.InvestmentRateRepository
 import br.edu.pablo.domain.entity.InvestmentRate
-import br.edu.pablo.domain.entity.InvestmentRateStatus
 import br.edu.pablo.domain.entity.InvestmentRateStatusParams
-import br.edu.pablo.usecase.constant.ONE_DAY
-import br.edu.pablo.usecase.constant.PERCENTAGE_RATE_MAX
-import br.edu.pablo.usecase.constant.PERCENTAGE_RATE_MIN
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import kotlin.random.Random
 
 @Service
 class InvestmentRateService(
@@ -17,25 +12,10 @@ class InvestmentRateService(
     val investmentRateStatusService: InvestmentRateStatusService
 ) {
 
-    fun getUpdatedRate(): InvestmentRate {
-        val newInvestmentRate = InvestmentRate(
-            null, "CDI",
-            getRandomPercentageRate(),
-            InvestmentRateStatus(null, InvestmentRateStatusParams.UPDATED.value),
-            LocalDate.now().minusDays(ONE_DAY)
-        )
-        updateToOutdatedStatus()
-        return save(newInvestmentRate)
-    }
-
     fun save(investmentRate: InvestmentRate): InvestmentRate {
         investmentRate.status = investmentRateStatusService.findByDescription(investmentRate.status.description).get()
         updateToOutdatedStatus()
         return investmentRateRepository.save(investmentRate)
-    }
-
-    fun saveAll(investmentRateList: MutableList<InvestmentRate>) {
-        investmentRateRepository.saveAll(investmentRateList)
     }
 
     fun findAll(): MutableList<InvestmentRate> {
@@ -43,8 +23,8 @@ class InvestmentRateService(
     }
 
     fun findEligibleToStatusUpdate(): MutableList<InvestmentRate> {
-        val yesterdayDateTime = LocalDate.now().atStartOfDay()
-        return investmentRateRepository.findInvestmentRateByUpdatedDateBefore(yesterdayDateTime)
+        val todayDateTime = LocalDate.now().atStartOfDay()
+        return investmentRateRepository.findInvestmentRateByUpdatedDateBefore(todayDateTime)
     }
 
     fun updateToOutdatedStatus() {
@@ -53,14 +33,9 @@ class InvestmentRateService(
         investmentsRateToUpdate.forEach {
             it.status = outdatedStatus
         }
-        saveAll(investmentsRateToUpdate)
-        /* investmentsRateToUpdate.forEach {
+        investmentsRateToUpdate.forEach {
             investmentRateRepository.save(it)
-        } */
-    }
-
-    fun getRandomPercentageRate(): Double {
-        return Random.nextDouble(PERCENTAGE_RATE_MIN, PERCENTAGE_RATE_MAX)
+        }
     }
 
     fun findInvestmentRateUpdated(): InvestmentRate {
