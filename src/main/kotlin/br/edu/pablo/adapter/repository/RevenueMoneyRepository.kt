4c135e6728fd72641e2cf938f0117c0816513e46
Pablo <pabloluiz.ribeiroo@gmail.com>
2022-06-30 22:12:15 -0300

    feat: total de rendimentos por cliente por ano

--- src/main/kotlin/br/edu/pablo/adapter/repository/RevenueMoneyRepository.kt
+++ src/main/kotlin/br/edu/pablo/adapter/repository/RevenueMoneyRepository.kt
@@ -3,10 +3,23 @@ package br.edu.pablo.adapter.repository
 import br.edu.pablo.domain.entity.Customer
 import br.edu.pablo.domain.entity.RevenueMoney
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.math.BigDecimal
+import java.time.LocalDateTime
 
 @Repository
 interface RevenueMoneyRepository : JpaRepository<RevenueMoney, Long> {
 
     fun findRevenueMoniesByCustomerAccountCustomer(customer: Customer): MutableList<RevenueMoney>
+
+    @Query(
+        value = "SELECT SUM(rm.amount) FROM RevenueMoney rm " +
+            "INNER JOIN CustomerAccount ca " +
+            "ON rm.customerAccount = ca " +
+            "INNER JOIN Customer c " +
+            "ON ca.customer = :customer " +
+            "WHERE rm.updatedDate BETWEEN :startOfYear AND :endOfYear"
+    )
+    fun findSumAmountByCustomer(customer: Customer, startOfYear: LocalDateTime, endOfYear: LocalDateTime): BigDecimal
 }
